2a54ebe1c62b9ab8eb2de586211dd3fb1120beb8
Timothy Maurer <timothygmaurer@outlook.com>
2024-11-11 15:35:32 +0100

    Add DbContext
    (not finished)

--- Src/DataAccess/YourGT.DataAccess.EFcore/YourGTDbContext.cs
+++ Src/DataAccess/YourGT.DataAccess.EFcore/YourGTDbContext.cs
@@ -1,8 +1,38 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Reflection.Metadata;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace YourGT.DataAccess.EFCore;
 
 public class YourGTDbContext : DbContext
 {
-    
+    private readonly IConfiguration _configuration;
+
+    public DbSet<Education> Educations
+    {
+        get; set;
+    }
+    public DbSet<Category> CategoryEntries
+    {
+        get; set;
+    }
+
+    //public DbSet<Comment> CommentEntries;
+
+    public WebBlogDbContext(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new BlogConfiguration());
+        modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        options.UseSqlServer(_configuration.GetConnectionString("Default"))
+            .EnableDetailedErrors();
+    }
 }
