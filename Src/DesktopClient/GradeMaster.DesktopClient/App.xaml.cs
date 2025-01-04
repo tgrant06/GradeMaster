53ddd5b55807eb803fb232598108d062d9fc4b9f
Timothy Maurer <timothygmaurer@outlook.com>
2025-01-04 02:15:16 +0100

    Add Reload Button to pages and update Style

--- Src/DesktopClient/GradeMaster.DesktopClient/App.xaml.cs
+++ Src/DesktopClient/GradeMaster.DesktopClient/App.xaml.cs
@@ -9,6 +9,6 @@ public partial class App : Application
 
     protected override Window CreateWindow(IActivationState? activationState)
     {
-        return new Window(new MainPage()) { Title = "GradeMaster", MinimumHeight = 600, MinimumWidth = 475 };
+        return new Window(new MainPage()) { Title = "GradeMaster", MinimumHeight = 600, MinimumWidth = 500 };
     }
 }
