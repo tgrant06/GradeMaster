c43486e848bfa3e4a8c84ac3dceddb9a67108cea
Timothy Maurer <timothygmaurer@outlook.com>
2025-01-14 13:32:50 +0100

    Add Migration and update styles

--- Src/DataAccess/GradeMaster.DataAccess.Core/Migrations/20250114110220_MigrationV6.Designer.cs
+++ Src/DataAccess/GradeMaster.DataAccess.Core/Migrations/20250114110220_MigrationV6.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using GradeMaster.DataAccess.Core;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GradeMaster.DataAccess.Core.Migrations
 {
     [DbContext(typeof(GradeMasterDbContext))]
-    partial class GradeMasterDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250114110220_MigrationV6")]
+    partial class MigrationV6
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.0");
