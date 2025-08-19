701c5c57876eae2db61c194023b888ec7027973d
Timothy Maurer <timothygmaurer@outlook.com>
2025-08-19 09:07:29 +0200

    Update AppJsonContext.cs

--- Src/Client/GradeMaster.DesktopClient/Json/AppJsonContext.cs
+++ Src/Client/GradeMaster.DesktopClient/Json/AppJsonContext.cs
@@ -6,16 +6,9 @@ namespace GradeMaster.DesktopClient.Json;
 /// JsonSerializerContext for serializing and deserializing <see cref="AppPreferencesObject"/>.
 /// Source generated.
 /// </summary>
-#if DEBUG
 [JsonSourceGenerationOptions(
     WriteIndented = true,
     PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase,
     GenerationMode = JsonSourceGenerationMode.Default)]
-#else
-[JsonSourceGenerationOptions(
-    WriteIndented = false,
-    PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase,
-    GenerationMode = JsonSourceGenerationMode.Default)]
-#endif
 [JsonSerializable(typeof(AppPreferencesObject))]
 internal partial class AppJsonContext : JsonSerializerContext;
